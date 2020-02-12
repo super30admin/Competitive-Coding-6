8bd573cdbd1b490bad60cfac7171693e0f10e36d
rutulusc <rutulbap@usc.edu>
2020-02-12 14:49:20 -0800

    Solved problem 81

---
+++ prob81.java
@@ -0,0 +1,31 @@
+// 784. Letter Case Permutation
+// Space complexity:exponential; O(2^n) where n is the string length (recursive stack)
+// Time complexity:exponential; O(2^n) where n is the string length
+class Solution {
+    public List<String> letterCasePermutation(String S) {
+        List<String> result=new ArrayList<>();
+        char [] carray=S.toCharArray();
+        backtrack(0,carray,result);
+        return result;
+    }
+    
+    private void backtrack(int index, char [] carray, List<String> result){
+        if(index==carray.length){
+            result.add(String.valueOf(carray));
+        }else if(!Character.isLetter(carray[index])){
+            backtrack(index+1,carray,result);
+        }else{
+            backtrack(index+1,carray,result);
+            if(Character.isLowerCase(carray[index])){
+                carray[index]=Character.toUpperCase(carray[index]);
+                backtrack(index+1,carray,result);
+                carray[index]=Character.toLowerCase(carray[index]);
+            }
+            else{
+                carray[index]=Character.toLowerCase(carray[index]);
+                backtrack(index+1,carray,result);
+                carray[index]=Character.toUpperCase(carray[index]);
+            }
+        }
+    }
+}
