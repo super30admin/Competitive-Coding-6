dac5d9610b7f43cfd6d0e4e88508dc8951d4424f
priyanka1012-singhal <priyanka.singhal@sjsu.edu>
2020-03-18 15:40:27 -0700

    Add Log Rate Limiter

--- LogLimiter.java
+++ LogLimiter.java
@@ -1,39 +1,20 @@
 
 // Your code here along with comments explaining your approach
-// Space - O(N) Time -  O(N!) where N! is the number of permutation
-class Solution {
-    boolean[] visited;
-    List<Set<Integer>> res;
-    public int countArrangement(int N) {
-        //edge case
-        if(N <= 0) return 0;
-        
-        visited = new boolean[N + 1];
-        res = new ArrayList<>();
-        backtrack(new HashSet<>(), 1, N);
-        return res.size();  
+//Time Complexity - O(1) and Space O(N) - where N is the size of unique messages
+public class Logger {
+    HashMap<String,Integer> msgTimestamp;
+    /** Initialize your data structure here. */
+    public Logger() {
+        msgTimestamp=new HashMap<>();
     }
     
-    private void backtrack(Set<Integer> temp, int position, int N){
-        // base case
-        if(position > N ){
-            res.add(new HashSet<Integer>(temp));
-            System.out.println(res);
-            return;
-        }
-        
-        //logic
-        for(int i = 1; i <= N; i++){
-            if(!temp.contains(i) && (i % position == 0 || position % i == 0)){
-                //action
-                
-                temp.add(i);
-                backtrack(temp, position + 1, N);
-                
-                temp.remove(temp.size() - 1);
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false. The timestamp is in seconds granularity. */
+        public boolean shouldPrintMessage(int timestamp, String message) {
+       
+            if(!msgTimestamp.containsKey(message)||timestamp - msgTimestamp.get(message)>=10){
+                msgTimestamp.put(message,timestamp);
+                return true;
             }
-            
+            return false;
         }
-        
-    }
-}
+     }
