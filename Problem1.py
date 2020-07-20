8b99e5db6ce96b5ff712352bcccb6ec2fb51e1ea
Shantanu609 <shantanu_shinde_@mail.fresnostate.edu>
2020-07-19 22:52:10 -0700

    Updated Problems with solution description and TC,SC for each

--- Problem1.py
+++ Problem1.py
@@ -1,12 +1,13 @@
-# Time Complexity : O(n^n) 
+# Time Complexity : O(n^n)
 # Space Complexity : O(n) 
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
 # Your code here along with comments explaining your approach
-
+# 1) For each number from 0 to n (1 extra for )
 class Solution:
+    temp = None
     def countArrangement(self, N: int) -> int:
-        
+        self.temp = [False] * (N+1)        
         def backtrack(pos,temp):
             # base
             if pos < 1: 
@@ -20,9 +21,10 @@ class Solution:
                     temp[i] = False 
             return count 
     
-        temp = [False] * (N+1)
-        return backtrack(N,temp)
+        
+        return backtrack(N,self.temp)
 
 if __name__ == "__main__":
     s = Solution()
     print(s.countArrangement(3))
+    print(s.temp)
