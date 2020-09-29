ff808ff71fcee4be4c5507cca1625fdf02807581
Laxmikant Bhaskar Pandhare <47066536+laxmikantbpandhare@users.noreply.github.com>
2020-09-28 17:34:35 -0700

    Update beautifulArrangement.java

--- beautifulArrangement.java
+++ beautifulArrangement.java
@@ -1,6 +1,6 @@
     /*  Explanation
     # Leetcode problem link : https://leetcode.com/problems/beautiful-arrangement/
-    Time Complexity for operators : o(n^n) .. n is the length of the string
+    Time Complexity for operators : o(n!) .. n is the length of the string
     Extra Space Complexity for operators : o(n) for (List<String> path) without recursive stack
     Did this code successfully run on Leetcode : NA
     Any problem you faced while coding this : No
