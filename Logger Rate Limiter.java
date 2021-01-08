11bf218747acf5d50bf749e18186ebe61e8ee7f4
Sarvesh Kulkarni <30865213+snk95@users.noreply.github.com>
2021-01-08 00:24:04 -0800

    Competitive-Coding-6 done

--- Logger Rate Limiter.java
+++ Logger Rate Limiter.java
@@ -21,26 +21,32 @@ class Logger:
         return True
 
         
+
+from collections import deque
 class Logger(object):
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self._msg_dict = {}
+        self._msg_set = set()
+        self._msg_queue = deque()
     
     def shouldPrintMessage(self, timestamp, message):
         """
         Returns true if the message should be printed in the given timestamp, otherwise returns false.
         """
-        if message not in self._msg_dict:
-            # case 1). add the message to print
-            self._msg_dict[message] = timestamp
-            return True
-
-        if timestamp - self._msg_dict[message] >= 10:
-            # case 2). update the timestamp of the message
-            self._msg_dict[message] = timestamp
+        while self._msg_queue:
+            msg, ts = self._msg_queue[0]
+            if timestamp - ts >= 10:
+                self._msg_queue.popleft()
+                self._msg_set.remove(msg)
+            else:
+                break
+        
+        if message not in self._msg_set:
+            self._msg_set.add(message)
+            self._msg_queue.append((message, timestamp))
             return True
         else:
             return False
