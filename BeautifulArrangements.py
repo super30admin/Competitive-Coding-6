40ce6347ad8059d69d3e8a4f40f7a376cca88d01
amby2964 <75859704+amby2964@users.noreply.github.com>
2021-02-01 00:37:36 -0700

    Update BeautifulArrangements.py

--- BeautifulArrangements.py
+++ BeautifulArrangements.py
@@ -1,20 +1,26 @@
-def beautifulArrangement(nums):
-    # Time Complexity: O(n!)
-    # Space Complexity: O(n)
-    if(len(nums)==0):
-        return 0
-
-    c = 0
+class Solution:
+    def countArrangement(self, n: int) -> int:
+        # Time Complexity: O(n!)
+        # Space Complexity: O(n)
+        nums = []
+        for i in range(0,n):
+            nums.append(i+1)
+            
+            
+        c = 0
     
-    def helper(nums, index, visited):
-        if(len(nums)+1==index):
-            nonlocal c
-            c+=1
-            return
-        for i in range(0,len(nums)):
-            if(nums[i]%index==0 or index%nums[i]==0) and not(nums[i] in visited):
-                visited.add(nums[i])
-                helper(nums, index+1, visited) # 2, [1]
-                visited.remove(nums[i])
-    helper(nums,1,set())
-    return c
+        def helper(nums, index, visited):
+            if(len(nums)+1==index):
+                nonlocal c
+                c+=1
+                return
+
+            for i in range(0,len(nums)):
+                if(nums[i]%index==0 or index%nums[i]==0) and not(nums[i] in visited):
+                    visited.add(nums[i])
+                    helper(nums, index+1, visited) # 2, [1]
+                    visited.remove(nums[i])
+
+
+        helper(nums,1,set())
+        return c
