e6f871c83f7e8a8e10205342dc0cbd66c7dcef09
Nitish Mane <nitish_mane90@yahoo.co.in>
2021-05-10 21:31:30 -0700

    Completed Competitive-Coding-6

---
+++ Logger_Rate_Limiter.py
@@ -0,0 +1,29 @@
+class Logger:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.s = {}
+        
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity.
+        """
+        if message not in self.s:
+            self.s[message] = timestamp
+            return True
+        else:
+            if (timestamp - self.s[message]) >=10:
+                self.s[message] = timestamp
+                return True
+            return False
+                
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
