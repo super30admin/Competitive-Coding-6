f74c6f32121262835effa0ce5eac08e48b9b4644
Adhish Shrivastava <adhish70@gmail.com>
2021-11-22 09:21:38 -0500

    Initial Commit

---
+++ Problem-83 Beautiful Arrangement.py
@@ -0,0 +1,11 @@
+# 526. Beautiful Arrangement
+# https://leetcode.com/problems/beautiful-arrangement/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:  
+    def countArrangement(self, n: int) -> int:
+        pass        
---
+++ Problem-84 Logger Rate Limiter.py
@@ -0,0 +1,20 @@
+# 359. Logger Rate Limiter
+# https://leetcode.com/problems/logger-rate-limiter/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Logger:
+
+    def __init__(self):
+        pass
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        pass
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
