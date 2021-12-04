76ec980781d6ad68485610df53da05dd16d160ea
Adhish Shrivastava <adhish70@gmail.com>
2021-12-04 16:28:21 -0500

    Problem84 done

--- Problem-84 Logger Rate Limiter.py
+++ Problem-84 Logger Rate Limiter.py
@@ -1,18 +1,23 @@
 # 359. Logger Rate Limiter
 # https://leetcode.com/problems/logger-rate-limiter/
 
-# Logic: 
-
-# Time Complexity: 
-# Space Complexity: 
+# Time Complexity: O(1)
+# Space Complexity: O(n)
 
 class Logger:
-
     def __init__(self):
-        pass
+        self.time_dict = dict()
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        pass
+        if message not in self.time_dict:
+            self.time_dict[message] = timestamp + 10
+            return True
+        else:
+            if self.time_dict[message] <= timestamp:
+                self.time_dict[message] = timestamp + 10
+                return True
+            else:
+                return False
 
 
 # Your Logger object will be instantiated and called as such:
