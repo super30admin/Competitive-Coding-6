0d0eba641a7896335a6462c04869e8166457e57d
Saumya <saumya071294@gmail.com>
2022-05-09 13:42:47 -0700

    Create BeautifulArrangement.py

---
+++ BeautifulArrangement.py
@@ -0,0 +1,18 @@
+# T.C. - O(n!) where n is the numbers 1...n
+# S.c. - O(n) --- recursions
+
+class Solution:
+    def countArrangement(self, n: int) -> int:
+        self.visited = [False]*(n+1)
+        self.count = 0
+
+        def calculate(n, pos):
+            if pos > n: self.count += 1
+            for i in range(1, n+1):
+                if not self.visited[i] and (pos % i == 0 or i%pos == 0):
+                    self.visited[i] = True
+                    calculate(n, pos+1)
+                    self.visited[i] = False
+
+        calculate(n, 1)
+        return self.count
