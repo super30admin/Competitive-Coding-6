4795029a80db5fddcdd7cb68144a2a92002a3dc9
Jayavani Akkiraju <jayavaniakkiraju@Jayavanis-MacBook-Pro.local>
2022-07-04 11:19:07 +0530

    Competitive-Coding-6 completed

--- beautifulArrangement.java
+++ beautifulArrangement.java
@@ -1,1 +1,34 @@
-
+// Passed test cases on leetcode
+class Solution {
+    // O(n!) - Time complexity where n! represnets number of permutations of the given input n
+    //O(n) - Space complexity to store auxilary array res[] here.
+    int cnt = 0;
+    void permut(int res[], int val, int n)
+    {
+            if(val > n)
+            {
+                cnt ++;
+                return;
+            }
+            
+            for(int i =1; i<= n; i++)
+            {
+                if(res[i] ==0 && (val % i ==0 || i % val ==0))
+                {
+                    res[i] = val;
+                    permut(res,val +1,n);
+                    res[i] = 0;
+                }
+            }
+    }
+        
+    
+    public int countArrangement(int n) {
+        
+        int res[] = new int[n+1];
+        permut(res, 1, n);
+        return cnt;
+    
+        
+    }
+}
--- loggerRate.py
+++ loggerRate.py
@@ -1,1 +1,24 @@
+'''
+-- Passed test cases on LeetCode
+'''
+class Logger:
+    #O(1) - Time complexity as lookup and update in hashmap takes constant time
+    #O(n) - space complexity as in n represents the number of messages which are to be stored in the hashmap
+    def __init__(self):
+        self.dict = {}
+        
 
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        if message not in self.dict:
+            self.dict[message] = timestamp
+            return True
+        if timestamp - self.dict[message] >= 10:
+            self.dict[message] = timestamp
+            return True
+        return False
+        
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
