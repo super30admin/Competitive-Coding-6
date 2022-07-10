32156ad26988048799112195dd90ca6c93b0853d
Mayank Goyani <mayankgoyani1@gmail.com>
2022-07-10 17:39:27 -0500

    Completed Competitive coding 6

--- logger.java
+++ logger.java
@@ -1,3 +1,8 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
 import java.util.*;
 
 class Logger {
