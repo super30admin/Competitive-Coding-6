d3045c99b62983952f3d169d5303e5b5daa78175
sohitrharma <rohit.sharma480@gmail.com>
2023-08-01 22:04:46 -0700

    Competitive-Coding-6

---
+++ LoggerRateLimiter.java
@@ -0,0 +1,90 @@
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+import java.util.HashMap;
+
+class Node {
+    public int timestamp;
+    public String message;
+    public Node prev;
+    public Node next;
+
+    public Node(int timestamp, String message) {
+        this.timestamp = timestamp;
+        this.message = message;
+    }
+
+}
+
+class Logger {
+    private static int Max_Time = 10;
+    private HashMap<String, Node> map;
+    private Node head;
+    private Node tail;
+
+    /** Initialize your data structure here. */
+
+    public Logger() {
+        map = new HashMap<>();
+        head = tail = new Node(0, "");
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    /**
+     * Returns true if the message should be printed in the given timestamp,
+     * otherwise returns false.
+     * 
+     * 
+     * 
+     * If this method returns false, the message will not be printed.
+     * 
+     * 
+     * 
+     * The timestamp is in seconds granularity.
+     */
+
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if (map.containsKey(message)) {
+            Node temp = map.get(message);
+            if (timestamp - temp.timestamp < Logger.Max_Time)
+                return false;
+            moveToHead(temp, timestamp);
+        } else {
+            if (map.size() >= Logger.Max_Time) {
+                removeTail();
+            }
+
+            addToHead(timestamp, message);
+        }
+        return true;
+    }
+
+    private void addToHead(int timestamp, String message) {
+        Node newNode = new Node(timestamp, message);
+        newNode.next = head.next;
+        head.next = newNode;
+        newNode.prev = head;
+        newNode.next.prev = newNode;
+        map.put(message, newNode);
+    }
+
+    private void removeTail() {
+        this.tail.prev.next = this.tail.next;
+        this.tail.next.prev = this.tail.prev;
+        map.remove(this.tail.message);
+    }
+
+    private void moveToHead(Node temp, int timestamp) {
+        temp.timestamp = timestamp;
+        temp.prev.next = temp.next;
+        temp.next.prev = temp.prev;
+        temp.next = head.next;
+        temp.prev = head;
+        head.next = temp;
+        temp.next.prev = temp;
+    }
+
+}
